cbfbe1d9329591418b71dcd013b4be11b840c26f
dudq2001 <dotrongdu@gmail.com>
2020-08-11 10:55:30 +0700

    Revert "bk"
    This reverts commit c1f98009e4dd5059be781ab1e3a9b39356673af4.

--- bookingg/database/Flights.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Illuminate\Database\Eloquent\Model;
-
-class Flights extends Model
-{
-    protected $fillable = [
-        'flightFrom', 'flightTo', 'flightDepart', 'flightReturn',
-    ];
-}
