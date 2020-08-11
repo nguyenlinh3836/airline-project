079ca7a8513756edd734ea1adf2c50cb3ee2b533
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2020-08-11 09:46:47 +0700

    fix giaodien

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-md bg-transparent">
+<nav class="navbar navbar-expand-md">
     <div class="container">
-        <a class="navbar-brand" href="">
+        <a class="navbar-brand" href="/">
             <img src="img/5f214561d2185.png"/>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarReponsive">
@@ -9,13 +9,13 @@
         <div class="collapse navbar-collapse" id="navbarReponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Trang chủ</a>
+                    <a class="nav-link" href="/">Trang chủ</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Đặt mua vé </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Thông tin hành trình</a>
+                    <a class="nav-link" href="Uudai">Ưu đãi hành trình</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="Gioithieu">Về chúng tôi</a>
